cf87cbab9379b12c704a01781972f45d47fe1373
eduardosmatheus <matheus-edu@outlook.com>
2021-01-12 17:51:43 -0300

    Initial commit; Create back-end scaffold

---
+++ github-tags-server/src/test/kotlin/com/eduardosmatheus/githubtagsserver/GithubTagsServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eduardosmatheus.githubtagsserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubTagsServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
