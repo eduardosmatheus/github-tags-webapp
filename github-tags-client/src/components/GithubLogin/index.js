038feafb670495c254f9f4991022278c13433128
eduardosmatheus <matheus-edu@outlook.com>
2021-01-13 09:43:46 -0300

    Create URL params converter.

--- github-tags-client/src/components/GithubLogin/index.js
+++ github-tags-client/src/components/GithubLogin/index.js
@@ -4,9 +4,28 @@ import { Button } from 'react-bootstrap'
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default class GithubLogin extends Component {
+  mountLoginAuthorizationParams = () => {
+    const defaultParams = {
+      client_id: 'b34d2a6fc9da4d853f0a',
+      redirect_url: 'http://localhost:3000/home'
+    };
+    return Object.keys(defaultParams).reduce((queryString, param) => {
+      const value = defaultParams[param];
+      if (!queryString.includes('?')) {
+        return `?${param}=${value}`;
+      }
+      return `${queryString}&${param}=${value}`;
+    }, '');
+  }
+
+  handleOpenAuth = () => {
+    const authParams = this.mountLoginAuthorizationParams();
+    window.open(`https://github.com/login/oauth/authorize${authParams}`);
+  }
+
   render() {
     return (
-      <Button variant="dark">
+      <Button variant="dark" onClick={this.handleOpenAuth}>
         <FontAwesomeIcon icon={faGithub} />
         {' '}
         Login with Github
