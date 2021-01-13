7c6ae0727659d71544e2d5d61cc456ef3447566e
eduardosmatheus <matheus-edu@outlook.com>
2021-01-13 18:38:25 -0300

    Setup default axios instance with authorization

---
+++ github-tags-client/src/api/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token');
+  const newConfig = config;
+  if (token) {
+    newConfig.headers.common.Authorization = `Bearer ${token}`;
+  }
+  return newConfig;
+});
+
+export default instance;
