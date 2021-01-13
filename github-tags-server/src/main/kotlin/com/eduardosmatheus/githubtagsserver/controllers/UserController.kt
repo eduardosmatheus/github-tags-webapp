0b70728fb3a106c4fdc7d9d88900a7158e4526a6
eduardosmatheus <matheus-edu@outlook.com>
2021-01-13 18:44:57 -0300

    Adjust users API

--- github-tags-server/src/main/kotlin/com/eduardosmatheus/githubtagsserver/controllers/UserController.kt
+++ github-tags-server/src/main/kotlin/com/eduardosmatheus/githubtagsserver/controllers/UserController.kt
@@ -1,11 +1,11 @@
 package com.eduardosmatheus.githubtagsserver.controllers
 
+import com.eduardosmatheus.githubtagsserver.services.GithubService
+import com.eduardosmatheus.githubtagsserver.services.GithubUser
 import com.eduardosmatheus.githubtagsserver.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/users")
@@ -14,6 +14,9 @@ class UserController {
     @Autowired
     private lateinit var userService: UserService
 
-    @GetMapping("/authorize")
-    fun authorize(@RequestParam code: String) = userService.authorize(code)
+    @PostMapping("/claim-access")
+    fun authorize(@RequestParam code: String) = userService.getClaims(code)
+
+    @GetMapping("/current")
+    fun getCurrentUser(authentication: Authentication) = authentication.principal as GithubUser
 }
