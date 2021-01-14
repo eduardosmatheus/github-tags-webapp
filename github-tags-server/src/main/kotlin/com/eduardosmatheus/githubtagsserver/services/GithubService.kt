46975573dfc904a81396480d1b9aab39a3dc73f0
eduardosmatheus <matheus-edu@outlook.com>
2021-01-14 20:51:38 -0300

    Move github behaviors to its corresponding place

--- github-tags-server/src/main/kotlin/com/eduardosmatheus/githubtagsserver/services/GithubService.kt
+++ github-tags-server/src/main/kotlin/com/eduardosmatheus/githubtagsserver/services/GithubService.kt
@@ -1,12 +1,12 @@
 package com.eduardosmatheus.githubtagsserver.services
 
+import com.eduardosmatheus.githubtagsserver.model.GithubUser
+import com.eduardosmatheus.githubtagsserver.security.UserClaims
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
+import org.springframework.http.*
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
@@ -20,16 +20,39 @@ object GithubService {
         val headers = HttpHeaders()
         headers["Authorization"] = token
         val entity = HttpEntity<Any>(headers)
-        val response = restClient.exchange(githubApiBaseURL, HttpMethod.GET, entity, GithubUser::class.java)
+        val response = restClient.exchange(githubApiBaseURL, HttpMethod.GET,
+            entity, GithubUser::class.java)
         return response.body
     }
-}
 
-@JsonDeserialize
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class GithubUser(
-    val login: String,
-    @JsonProperty("avatar_url")
-    val avatarUrl: String,
-    val email: String
-)
+    /**
+     * TODO: Mover para variáveis de ambiente, em prol de não commitar publicamente.
+     */
+    private val githubBaseURL = "https://github.com/login/oauth/access_token"
+    private val client_id = "b34d2a6fc9da4d853f0a"
+    private val client_secret = "bf7914d3bce7e9a5cb6ec9eb9126cc6eeafc8bc3"
+    private val BAD_VERIFICATION_CODE = "bad_verification_code"
+
+    fun getClaims(code: String): ResponseEntity<Any> {
+        val response = getGithubAuthorization(code)
+        val errorCode = response["error"]
+        if (errorCode != null && errorCode.asText() == BAD_VERIFICATION_CODE) {
+            return ResponseEntity(response, HttpStatus.FORBIDDEN)
+        }
+        return ResponseEntity(response, HttpStatus.OK)
+    }
+
+    private fun getGithubAuthorization(code: String): JsonNode {
+        val client = RestTemplate()
+        val headers = HttpHeaders()
+        headers["Accept"] = "application/json"
+        val entity = HttpEntity<UserClaims>(headers)
+        val response = client.exchange(
+            "$githubBaseURL?client_id=${client_id}&client_secret=${client_secret}&code=$code",
+            HttpMethod.POST,
+            entity,
+            JsonNode::class.java
+        )
+        return response.body!!
+    }
+}
--- github-tags-server/src/main/kotlin/com/eduardosmatheus/githubtagsserver/services/UserService.kt
+++ github-tags-server/src/main/kotlin/com/eduardosmatheus/githubtagsserver/services/UserService.kt
@@ -1,49 +1,11 @@
 package com.eduardosmatheus.githubtagsserver.services
 
-import com.eduardosmatheus.githubtagsserver.security.UserClaims
-import com.fasterxml.jackson.databind.JsonNode
-import org.springframework.http.*
+import com.eduardosmatheus.githubtagsserver.model.User
+import com.eduardosmatheus.githubtagsserver.repositories.UsersRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
 
 @Service
 class UserService {
-
-    private val githubBaseURL = "https://github.com/login/oauth/access_token"
-    private val client_id = "b34d2a6fc9da4d853f0a"
-    private val client_secret = "bf7914d3bce7e9a5cb6ec9eb9126cc6eeafc8bc3"
-    private val BAD_VERIFICATION_CODE = "bad_verification_code"
-
-    fun getClaims(code: String): ResponseEntity<Any> {
-        val (client, entity) = githubClient()
-        val response = getGithubAuthorization(client, code, entity)
-        val errorCode = response["error"]
-        if (errorCode != null && errorCode.asText() == BAD_VERIFICATION_CODE) {
-            return ResponseEntity(response, HttpStatus.FORBIDDEN)
-        }
-        return ResponseEntity(response, HttpStatus.OK)
-    }
-
-    private fun githubClient(): Pair<RestTemplate, HttpEntity<UserClaims>> {
-        val client = RestTemplate()
-        val headers = HttpHeaders()
-        headers["Accept"] = "application/json"
-        val entity = HttpEntity<UserClaims>(headers)
-        return Pair(client, entity)
-    }
-
-    private fun getGithubAuthorization(
-        client: RestTemplate,
-        code: String,
-        entity: HttpEntity<UserClaims>
-    ): JsonNode {
-        val response = client.exchange(
-            "$githubBaseURL?client_id=${client_id}&client_secret=${client_secret}&code=$code",
-            HttpMethod.POST,
-            entity,
-            JsonNode::class.java
-        )
-        return response.body!!
-    }
 
 }
