3faf3b0321f7f78fa885bcab7ca9337a24e8185c
eduardosmatheus <matheus-edu@outlook.com>
2021-01-14 08:43:00 -0300

    Fix login UI

--- github-tags-client/src/containers/Login/index.js
+++ github-tags-client/src/containers/Login/index.js
@@ -1,12 +1,21 @@
 import React, { useEffect } from 'react'
+import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
 import GithubLogin from '../../components/GithubLogin'
 
 export default function Login() {
   useEffect(() => { localStorage.clear() }, []);
   return (
-    <div className="App">
-      Bem vindo ao Criador de Tags para busca no Github!
-      <GithubLogin />
-    </div>
+    <Container>
+      <Jumbotron>
+        <Row className="justify-content-between">
+          <Col sm={12} xs={12} md={9}>
+            <h3>Bem vindo ao Criador de Tags para busca no Github!</h3>
+          </Col>
+          <Col sm={12} xs={12} md={3}>
+            <GithubLogin />
+          </Col>
+        </Row>
+      </Jumbotron>
+    </Container>
   )
 }
