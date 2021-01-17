aa406a684711cc2b9162515ffba211d1f20b4d2a
eduardosmatheus <matheus-edu@outlook.com>
2021-01-16 21:03:39 -0300

    Partial commit; Create "Sign Up" screen

--- github-tags-client/src/App.js
+++ github-tags-client/src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 import Home from './containers/Home';
 import Login from './containers/Login';
+import SignUp from './containers/SignUp';
 
 function App() {
   return (
     <Switch>
       <Route path="/login" component={Login} />
+      <Route path="/sign-up" component={SignUp} />
       <Route path="/" component={Home} />
     </Switch>
   );
