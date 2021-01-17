50548ce39035dbfda7503d52b1137fd49cd38f35
eduardosmatheus <matheus-edu@outlook.com>
2021-01-17 20:32:43 -0300

    Remove unecessary routes

--- github-tags-client/src/App.js
+++ github-tags-client/src/App.js
@@ -3,17 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 import Home from './containers/Home';
 import Login from './containers/Login';
-import SignUp from './containers/SignUp';
 
 function App() {
   return (
-    <div className="main-app-container">
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/sign-up" component={SignUp} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/" component={Home} />
+    </Switch>
   );
 }
 
