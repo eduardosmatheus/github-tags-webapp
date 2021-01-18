27a846932a49b343dafec12531116f44ed828b50
eduardosmatheus <matheus-edu@outlook.com>
2021-01-18 14:12:06 -0300

    View current selected tags on each repository.

--- github-tags-client/src/containers/Repositories/RepositoryList/index.js
+++ github-tags-client/src/containers/Repositories/RepositoryList/index.js
@@ -12,7 +12,13 @@ function Repository({ full_name, html_url, description, tags, onTagsEdit }) {
       </Card.Header>
       <Card.Body className={Styles.RepositoryContent}>
         <Card.Text className={Styles.RepoDescription}>{description}</Card.Text>
-        {tags.map(repoTag => <Badge variant="light">{repoTag.tag.name}</Badge>)}
+        <div className={Styles.RepositoryTagsList}>
+          {tags.map((repoTag, idx) => (
+            <Badge key={idx} variant="primary" className={Styles.RepositoryTag}>
+              {repoTag.tag.name}
+            </Badge>
+          ))}
+        </div>
         <Button variant="outline-success" onClick={onTagsEdit}>
           <FontAwesomeIcon icon={faTags} />
         </Button>
