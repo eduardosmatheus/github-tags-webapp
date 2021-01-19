5d7f442b9943ea7e57b4a87cd31efdbe8333a8a1
eduardosmatheus <matheus-edu@outlook.com>
2021-01-18 22:24:22 -0300

    Test "at least a thing on screen"; Mock repositories with tags

--- github-tags-client/src/components/FuzzyFinder/__tests__/index.test.js
+++ github-tags-client/src/components/FuzzyFinder/__tests__/index.test.js
@@ -1,16 +1,17 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import FuzzyFinder from '../index';
-
 import { render, fireEvent, screen } from '@testing-library/react';
+import FuzzyFinder from '../index';
+import reposMock from './reposMock.json';
+import { Repository } from '../../../pages/Repositories/RepositoryList';
 
 test('shows the whole list when nothing is typed', () => {
   render(
     <FuzzyFinder
-      data={[]}
+      data={reposMock}
+      rowRenderer={Repository}
+      keyExtractor={row => row.id}
     />
   );
-  expect(screen.queryAllByText('')).toBe(123);
-
-  
-})
+  expect(screen.queryByText('retrofit')).toBeInTheDocument();  
+});
