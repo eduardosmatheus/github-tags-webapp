32f172d197435795d3ff2fc85d61860253084bdc
eduardosmatheus <matheus-edu@outlook.com>
2021-01-19 18:54:45 -0300

    Remove unnecessary filter chain components.

--- github-tags-server/src/main/kotlin/com/eduardosmatheus/githubtagsserver/config/WebSecurityConfig.kt
+++ github-tags-server/src/main/kotlin/com/eduardosmatheus/githubtagsserver/config/WebSecurityConfig.kt
@@ -1,15 +1,10 @@
 package com.eduardosmatheus.githubtagsserver.config
 
-import com.auth0.jwt.algorithms.Algorithm
-import com.eduardosmatheus.githubtagsserver.security.AuthenticationFilter
 import com.eduardosmatheus.githubtagsserver.security.AuthorizationFilter
-import com.eduardosmatheus.githubtagsserver.security.SystemUserAuthProvider
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -26,9 +21,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 
-    @Autowired
-    private lateinit var authProvider: SystemUserAuthProvider
-
     @Throws(Exception::class)
     override fun configure(httpSecurity: HttpSecurity) {
         httpSecurity.sessionManagement()
@@ -37,22 +29,11 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
             .csrf().disable()
             .authorizeRequests()
             .antMatchers(HttpMethod.OPTIONS).permitAll()
-            .antMatchers(HttpMethod.POST, "/login").permitAll()
             .antMatchers(HttpMethod.POST, "/api/users").permitAll()
             .antMatchers(HttpMethod.POST, "/api/users/claim-access").permitAll()
             .anyRequest().authenticated()
             .and()
-            .addFilter(AuthenticationFilter(authenticationManagerBean()))
             .addFilterBefore(AuthorizationFilter(), BasicAuthenticationFilter::class.java)
-    }
-
-    @Bean
-    override fun authenticationManagerBean(): AuthenticationManager {
-        return super.authenticationManagerBean()
-    }
-
-    override fun configure(auth: AuthenticationManagerBuilder?) {
-        auth?.authenticationProvider(authProvider)
     }
 
     @Bean
@@ -74,7 +55,4 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Bean
     fun bCryptPasswordEncoder() = BCryptPasswordEncoder()
-
-    @Bean
-    fun algorithm() = Algorithm.HMAC256("MySecret")
 }
