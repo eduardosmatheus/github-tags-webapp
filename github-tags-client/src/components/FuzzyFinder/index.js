a4d83c25cd4d0e11e5c51fe187a1589a757c2f3c
eduardosmatheus <matheus-edu@outlook.com>
2021-01-18 22:25:42 -0300

    Implement items display; Implement hint-based filtering; Save filtered data while searching.

--- github-tags-client/src/components/FuzzyFinder/index.js
+++ github-tags-client/src/components/FuzzyFinder/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FormControl } from 'react-bootstrap';
 import Styles from './index.module.scss';
 
-export default function FuzzyFinder({ data }) {
+export default function FuzzyFinder({
+  data,
+  rowRenderer: RowRenderer,
+  keyExtractor,
+  getHint
+}) {
+  const [searchHint, setSearchHint] = useState('');
+  const [filteredData, setFilteredData] = useState(null);
+
+  const handleSearchHint = (hint) => {
+    setSearchHint(hint);
+    const filtered = data.filter(item => {
+      return getHint(item).toString().includes(hint);
+    });
+    setFilteredData(filtered);
+  };
+
+  const displayList = searchHint && filteredData && filteredData.length
+    ? filteredData : data;
+
   return (
     <div className={Styles.FuzzyFinderContainer}>
-      
+      <FormControl
+        type="text"
+        name="searchHint"
+        value={searchHint}
+        onChange={e => handleSearchHint(e.target.value)}
+      />
+      <div className={Styles.FuzzyFinderResults}>
+        {displayList.map(item => (
+          <RowRenderer key={keyExtractor(item)} {...item} />
+        ))}
+      </div>
     </div>
   )
 }
