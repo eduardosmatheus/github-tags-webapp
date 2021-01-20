ef8cc56aff821da1032c0965ae917cfa6ff1a3e8
eduardosmatheus <matheus-edu@outlook.com>
2021-01-20 10:26:48 -0300

    Create tags service; Fix unique constraint exception handling.

--- src/main/kotlin/com/eduardosmatheus/githubtagsserver/controllers/TagsController.kt
+++ src/main/kotlin/com/eduardosmatheus/githubtagsserver/controllers/TagsController.kt
@@ -3,6 +3,7 @@ package com.eduardosmatheus.githubtagsserver.controllers
 import com.eduardosmatheus.githubtagsserver.model.Tag
 import com.eduardosmatheus.githubtagsserver.repositories.GithubRepoTagsRepository
 import com.eduardosmatheus.githubtagsserver.repositories.TagsRepository
+import com.eduardosmatheus.githubtagsserver.services.TagsService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -15,25 +16,17 @@ class TagsController {
 	@Autowired
 	private lateinit var tagsRepository: TagsRepository
 	@Autowired
-	private lateinit var githubRepoTagsRepository: GithubRepoTagsRepository
+	private lateinit var tagsService: TagsService
 
 	@GetMapping
 	fun findAll() = tagsRepository.findAll()
 
 	@PostMapping
-	fun create(@RequestBody payload: Tag) = tagsRepository.save(payload)
+	fun create(@RequestBody payload: Tag) = tagsService.create(payload)
 
 	@PutMapping
 	fun update(@RequestBody payload: Tag) = tagsRepository.save(payload)
 
 	@DeleteMapping("/{id}")
-	fun delete(@PathVariable id: Int) {
-		val existingAssociations = githubRepoTagsRepository.findByTagId(id)
-		if (existingAssociations.isNotEmpty()) {
-			throw ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY,
-				"Não é possível remover tags que ainda possuem associações."
-			)
-		}
-		tagsRepository.deleteById(id)
-	}
+	fun delete(@PathVariable id: Int) = tagsService.delete(id)
 }
---
+++ src/main/kotlin/com/eduardosmatheus/githubtagsserver/services/TagsService.kt
@@ -0,0 +1,41 @@
+package com.eduardosmatheus.githubtagsserver.services
+
+import com.eduardosmatheus.githubtagsserver.model.Tag
+import com.eduardosmatheus.githubtagsserver.repositories.GithubRepoTagsRepository
+import com.eduardosmatheus.githubtagsserver.repositories.TagsRepository
+import org.hibernate.exception.ConstraintViolationException
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+class TagsService {
+
+	@Autowired
+	private lateinit var tagsRepository: TagsRepository
+	@Autowired
+	private lateinit var githubRepoTagsRepository: GithubRepoTagsRepository
+
+	fun create(tag: Tag): Tag {
+		return try {
+			tagsRepository.save(tag)
+		} catch (e: ConstraintViolationException) {
+			throw ResponseStatusException(
+				HttpStatus.UNPROCESSABLE_ENTITY,
+				"Já existe uma tag com esse nome."
+			)
+		}
+	}
+
+	fun delete(id: Int) {
+		val existingAssociations = githubRepoTagsRepository.findByTagId(id)
+		if (existingAssociations.isNotEmpty()) {
+			throw ResponseStatusException(
+				HttpStatus.UNPROCESSABLE_ENTITY,
+				"Não é possível remover tags que ainda possuem associações."
+			)
+		}
+		tagsRepository.deleteById(id)
+	}
+}
